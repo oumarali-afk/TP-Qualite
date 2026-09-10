6c6150b93386f27bd08b3889a96892afb0e75189
Antoine <antoinemorado17@gmail.com>
2026-09-10 10:42:27 +0200

    ajout de sub

--- mon-projet/src/test/java/com/monentreprise/app/AppTest.java
+++ mon-projet/src/test/java/com/monentreprise/app/AppTest.java
@@ -1,5 +1,6 @@
 package com.monentreprise.app;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
@@ -16,5 +17,15 @@ public class AppTest
     public void shouldAnswerWithTrue()
     {
         assertTrue( true );
+    }
+
+    @Test 
+    public void TestSub(){
+        int a = 10;
+        int b = 4;
+        int attendu = 6;
+        
+        int resultat = App.sub(a, b);
+        assertEquals(attendu, resultat,"la soustraction devrait donner 6");
     }
 }
