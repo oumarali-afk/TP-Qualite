07961bcd315bbd9acfa9c94a3feda77e7c38ff45
Antoine <antoinemorado17@gmail.com>
2026-09-10 10:33:39 +0200

    ajout de la structure maven

---
+++ mon-projet/src/main/java/com/monentreprise/app/App.java
@@ -0,0 +1,13 @@
+package com.monentreprise.app;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
