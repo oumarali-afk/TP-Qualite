2302d1df67380b9224b9bda38c889b1cbbaa9caf
oumarali-afk <saykhanovoumarali@gmail.com>
2026-09-10 11:18:45 +0200

    Ajout du message d'addition

--- mon-projet/src/main/java/Main.java
+++ mon-projet/src/main/java/Main.java
@@ -2,5 +2,5 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Calculatrice");
-    }
-}
+        System.out.println("Addition fonctionnelle dans la calculatrice");
+   }
